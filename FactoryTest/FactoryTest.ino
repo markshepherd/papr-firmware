42d4e5c6f4a2d182d20cc067e93aa7df1c13e091
markshepherd <mark.e.shepherd@gmail.com>
2020-12-11 14:52:11 -0800

    Initial checkin of FactoryTest app

---
+++ FactoryTest/FactoryTest.ino
@@ -0,0 +1,133 @@
+/*
+*   FactoryTest.ino
+*
+* This file is the main program for factory testing of the v2 PAPR board.
+*
+*/
+
+#include "PAPRHwDefs.h"
+#include "ButtonDebounce.h"
+
+byte LEDpins[] = {
+    BATTERY_LED_1_PIN,
+    BATTERY_LED_2_PIN,
+    BATTERY_LED_3_PIN,
+    Error_LED_PIN,
+    MODE_LED_1_PIN,
+    MODE_LED_2_PIN,
+    MODE_LED_3_PIN
+};
+int numLEDs = sizeof(LEDpins) / sizeof(byte);
+
+int currentLED = -1;
+
+#define DELAY_10ms 10
+#define DELAY_100ms 100
+#define DELAY_500ms 500
+#define DELAY_1SEC 1000
+#define DELAY_5SEC 5000
+
+bool Fan_Up_Value;
+bool Fan_Down_Value;
+bool Monitor_PIN_Value;
+
+uint8_t s_fan_pwm_off = 0;
+uint8_t s_fan_pwm_low = 51;
+uint8_t s_fan_pwm_mid_low = 102;
+uint8_t s_fan_pwm_mid = 153;
+uint8_t s_fan_pwm_mid_hi = 204;
+uint8_t s_fan_pwm_hi = 255;
+
+int iSpeed = 0;
+
+uint8_t speeds[] = {
+    s_fan_pwm_off,
+    s_fan_pwm_low,
+    s_fan_pwm_mid_low,
+    s_fan_pwm_mid,
+    s_fan_pwm_mid_hi,
+    s_fan_pwm_hi,
+};
+const int cSpeeds = sizeof(speeds) / sizeof(uint8_t);
+
+ButtonDebounce buttonFanUp(FAN_UP_PIN, 100);
+ButtonDebounce buttonFanDown(FAN_DOWN_PIN, 100);
+
+//================================================================
+
+void flashLED(byte pin, unsigned long duration) {
+    digitalWrite(pin, LOW);
+    delay(duration);
+    digitalWrite(pin, HIGH);
+}
+
+void flashLEDs(unsigned long duration) {
+    // Turn on all LEDs
+    for (int i = 0; i < numLEDs; i += 1) {
+        digitalWrite(LEDpins[i], LOW);
+    }
+
+    delay(duration);
+
+    // Turn off all LEDs
+    for (int i = 0; i < numLEDs; i += 1) {
+        digitalWrite(LEDpins[i], HIGH);
+    }
+
+    delay(duration);
+}
+
+void onButtonUpChange(const int state) {
+    if (state == HIGH) {
+        currentLED += 1;
+        if (currentLED >= numLEDs) currentLED = 0;
+        flashLED(LEDpins[currentLED], 1000);
+    }
+}
+
+void onButtonDownChange(const int state) {
+    if (state == HIGH) {
+        currentLED -= 1;
+        if (currentLED < 0) currentLED = numLEDs - 1;
+        flashLED(LEDpins[currentLED], 1000);
+    }
+}
+
+void flashUsartState() {
+    int receiveEnabled = bitRead(UCSR0B, RXEN0);
+    int transmitEnabled = bitRead(UCSR0B, TXEN0);
+    delay(2000);
+    if (receiveEnabled) {
+        flashLEDs(100);
+    }
+    else {
+        flashLEDs(100);
+        flashLEDs(100);
+    }
+    delay(2000);
+    if (transmitEnabled) {
+        flashLEDs(100);
+    }
+    else {
+        flashLEDs(100);
+        flashLEDs(100);
+    }
+}
+void setup() {
+    configurePins();
+    initializeDevices();
+
+    // Initialize the button scanners
+    buttonFanUp.setCallback(onButtonUpChange);
+    buttonFanDown.setCallback(onButtonDownChange);
+
+    // Flash all LEDs a few times
+    for (int k = 0; k < 3; k += 1) {
+        flashLEDs(200);
+    }
+}
+
+void loop() {
+    buttonFanUp.update();
+    buttonFanDown.update();
+}
---
+++ FactoryTest/PAPRHwDefs.h
@@ -0,0 +1,74 @@
+#pragma once
+
+/*
+*   PAPRHwDefs.h
+* 
+* This header file defines all the input/output pins on the v2 PAPR board,
+* and provides code to configure the pins and initialize them to a default state.
+*/
+
+
+//================================================================
+// OUTPUT PINS
+#define FAN_PWM_PIN                 5   /* PD5 */  /* Analog 0-255 duty cycle */
+#define BATTERY_LED_1_PIN           A0  /* PC0 */  /* LOW = on, HIGH = off */
+#define BATTERY_LED_2_PIN           A1  /* PC1 */  /* LOW = on, HIGH = off */
+#define BATTERY_LED_3_PIN           A2  /* PC2 */  /* LOW = on, HIGH = off */
+#define Error_LED_PIN               A3  /* PC3 */  /* LOW = on, HIGH = off */
+#define MODE_LED_1_PIN              A4  /* PC4 */  /* LOW = on, HIGH = off */
+#define MODE_LED_2_PIN              A5  /* PC5 */  /* LOW = on, HIGH = off */
+#define MODE_LED_3_PIN              0   /* PD0 */  /* LOW = on, HIGH = off */
+#define BUZZER_PIN                  10  /* PB2 */
+#define VIBRATOR_PIN                2   /* PD2 */
+
+//================================================================
+// INPUT PINS
+#define BATTERY_VOLTAGE_PIN         A7  /* PC7, ADC7 */
+#define Monitor_PIN                 7   /* PD7 */  /* LOW = power down button pushed */
+#define FAN_RPM_PIN                 3   /* PD3 */  /* Digital square wave, frequency proportional to RPM */
+#define FAN_UP_PIN                  1   /* PD1 */  /* LOW = pushed, HIGH = released */
+#define FAN_DOWN_PIN                9   /* PB1 */  /* LOW = pushed, HIGH = released */
+
+//================================================================
+inline void configurePins() {
+    // Output pins require a 1 bit
+    DDRB = DDRB | B00000100; // Outputs are PB2
+    DDRC = DDRC | B00111111; // Outputs are PC0, PC1, PC2, PC3, PC4, PC5
+    DDRD = DDRD | B00100101; // Outputs are PD0, PD2, PD5
+
+    // Input pins require a 0 bit
+    DDRB = DDRB & B11111101; // Inputs are PB1
+    DDRC = DDRC & B01111111; // Inputs are PC7
+    DDRD = DDRD & B01110101; // Inputs are PD1, PD3, PD7
+
+    // previous code
+    //    DDRB = DDRB | B01000101; // erroneously defines PB0 and PB6 as outputs.
+    //    DDRD = DDRD | B10000101; // erroneously defines PD7 as output
+    //    fails to define as output PD5 
+    //    assumes all inputs are already set properly to 0 in DDRB/C/D
+
+    // Setup Pullup resistor for PD7
+    PORTD = B10000000;
+
+    // Turn off the USART, because it wants to use pins 0 and 1 (a.k.a. PORTD0 and PORTD1) which we are using for other things.
+    // We never call Serial.begin() so I don't know why the USART is on.
+    bitClear(UCSR0B, RXEN0);
+    bitClear(UCSR0B, TXEN0);
+    bitClear(UCSR0B, RXCIE0);
+    bitClear(UCSR0B, UDRIE0);
+}
+
+inline void initializeDevices() {
+    // Fan to lowest speed
+    analogWrite(FAN_PWM_PIN, 0);
+
+    // All LEDs off
+    PORTC = 0x3f;
+    PORTD = 0x01;
+
+    // Buzzer off
+    analogWrite(BUZZER_PIN, 0);
+
+    // Vibrator off
+    digitalWrite(VIBRATOR_PIN, LOW);
+}
