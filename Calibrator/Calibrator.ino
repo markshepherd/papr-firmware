eab49978f84050d8f2c78b5f72a90c697bb2ad2a
markshepherd <mark.e.shepherd@gmail.com>
2021-04-25 13:42:04 -0700

    update calibrator for latest rev 3.1 board

--- Calibrator/Calibrator.ino
+++ Calibrator/Calibrator.ino
@@ -12,20 +12,20 @@ int currentDutyCycle;
 
 class Sampler {
 public:
-    unsigned long highest;
-    unsigned long lowest;
-    unsigned long accumulator;
-    unsigned long sampleCount;
+    long highest;
+    long lowest;
+    long accumulator;
+    long sampleCount;
 
     void reset()
     {
-        highest = 0;
-        lowest = ULONG_MAX;
+        highest = LONG_MIN;
+        lowest = LONG_MAX;
         accumulator = 0;
         sampleCount = 0;
     }
 
-    void sample(unsigned long value)
+    void sample(long value)
     {
         if (value < lowest) lowest = value;
         if (value > highest) highest = value;
@@ -33,14 +33,9 @@ public:
         sampleCount += 1;
     }
 
-    unsigned long average()
+    long average()
     {
         return sampleCount > 0 ? (accumulator / sampleCount) : 0;
-    }
-
-    double daverage()
-    {
-        return sampleCount > 0 ? (((double)accumulator) / ((double)sampleCount)) : 0;
     }
 };
 
@@ -50,6 +45,8 @@ Sampler voltage;
 Sampler rpm;
 Sampler current;
 Sampler rawRPM;
+Sampler referenceVoltage;
+Sampler computedCurrent;
 
 bool toneOn = false;
 
@@ -95,17 +92,20 @@ void beginSamplePeriod()
     rpm.reset();
     current.reset();
     rawRPM.reset();
+    referenceVoltage.reset();
+    computedCurrent.reset();
 }
 
 void takeSample()
 {
     unsigned int batteryLevel = analogRead(BATTERY_VOLTAGE_PIN);
-    unsigned int chargeCurrent = analogRead(CHARGE_CURRENT_PIN);
 
     rpm.sample(fanController.getSpeed());
     rawRPM.sample(digitalRead(FAN_RPM_PIN));
     voltage.sample(batteryLevel);
-    current.sample(chargeCurrent);
+    referenceVoltage.sample(analogRead(REFERENCE_VOLTAGE_PIN));
+    current.sample(analogRead(CHARGE_CURRENT_PIN));
+    computedCurrent.sample((long)(Hardware::instance.readCurrent() * 1000000.0));
 
     // Do a low pass filter on the battery level.
     // I have found that the filtered battery level varies +/- 2 ADC units, which is much better
@@ -118,28 +118,31 @@ void endSamplePeriod()
 {
     if (!skipReport) {
         float batteryVolts = voltage.average() * VOLTS_PER_VOLTAGE_UNIT;
-
-        long chargeFlow = current.average(); // 0 to 1023, < 512 means charging, the units are arbitrary "charge flow units"
-        chargeFlow -= 512; // shift the range to -512(charging) to +511(discharging)
-        double chargeFlowAmps = ((double)-chargeFlow) * AMPS_PER_CHARGE_FLOW_UNIT; // convert to amps, and flip so that > 0 means charging
-
         char buffer1[50];
         char buffer2[50];
-        serialPrintf("Duty cycle %d, RPM min %lu, avg %lu, max %lu, VOLTAGE min %lu, avg %lu, max %lu, filt %s, comp %s, CURRENT min %lu, avg %lu, max %lu, comp %s, TONE %s, samples %lu",
+        serialPrintf("Duty cycle %d, RPM min %l, avg %l, max %l, VOLTAGE min %l, avg %l, max %l, filt %s, comp %s,\
+ CURRENT min %l, avg %l, max %l, REF min %l, avg %l, max %l, �AMPS min %l, avg %l, max %l, TONE %s, samples %lu",
             currentDutyCycle,
             rpm.lowest, rpm.average(), rpm.highest,
             voltage.lowest, voltage.average(), voltage.highest, renderDouble(filteredBatteryLevel), renderDouble(batteryVolts, buffer1),
-            current.lowest, current.average(), current.highest, renderDouble(chargeFlowAmps, buffer2),
+            current.lowest, current.average(), current.highest,
+            referenceVoltage.lowest, referenceVoltage.average(), referenceVoltage.highest,
+            computedCurrent.lowest, computedCurrent.average(), computedCurrent.highest,
             toneOn ? "on" : "off", rpm.sampleCount);
     }
 }
 
 void setFanDutyCycle(int dutyCycle)
 {
-    if (dutyCycle < 0) dutyCycle = 0;
+    if (dutyCycle < -10) dutyCycle = -10;
     if (dutyCycle > 100) dutyCycle = 100;
     currentDutyCycle = dutyCycle;
-    fanController.setDutyCycle(dutyCycle);
+    if (currentDutyCycle >= 0) {
+        digitalWrite(FAN_ENABLE_PIN, FAN_ON);
+        fanController.setDutyCycle(dutyCycle);
+    } else {
+        digitalWrite(FAN_ENABLE_PIN, FAN_OFF);
+    }
     beginSamplePeriod();
     skipReport = true;
     serialPrintf("Duty cycle %d\r\n\r\n", dutyCycle);
@@ -199,12 +202,10 @@ PowerOnButtonInterruptCallback powerOnButtonInterruptCallback;
 
 void setup()
 {
-    Hardware::instance.setPowerMode(fullPowerMode);
-    Hardware::instance.configurePins();
-    Hardware::instance.initializeDevices();
+    Hardware::instance.setup();
     initializeSerial();
     fanController.begin();
-    setFanDutyCycle(0);
+    setFanDutyCycle(-10);
     flashLEDs(500, 3);
     digitalWrite(FAN_LOW_LED_PIN, LED_ON);
     digitalWrite(BATTERY_LED_MED_PIN, LED_ON);
